04d1b47f7bdf5efe31715168867a50f90a71b1ed
squirrelfeeder <jennifer.eel@me.com>
2016-03-26 21:12:59 -0500

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
 # code your #position_taken? method here!
+def position_taken?(board, space)
+  if (board[space] == " " || board[space] == "" || board[space] == nil)
+    return false
+  elsif (board[space] == "X" || board[space] == "O")
+    return true
+  end
+end
